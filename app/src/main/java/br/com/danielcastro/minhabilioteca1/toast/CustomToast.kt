f29bcc0b06d19a2ad3beac40a8509126fa75e2e4
danielcs16 <danielcs1602@gmail.com>
2018-10-16 22:51:09 -0300

    Criacao do Toast customizado de sucesso

--- app/src/main/java/br/com/danielcastro/minhabilioteca1/toast/CustomToast.kt
+++ app/src/main/java/br/com/danielcastro/minhabilioteca1/toast/CustomToast.kt
@@ -1,6 +1,7 @@
 package br.com.danielcastro.minhabilioteca1.toast
 
 import android.content.Context
+import android.graphics.Color
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.widget.TextView
@@ -15,6 +16,21 @@ class CustomToast(val context: Context, val inflater: LayoutInflater) {
 
         val textView = customToastroot.findViewById<TextView>(R.id.textView1)
         textView.text = text
+
+        customtoast.view = customToastroot
+        customtoast.setGravity(Gravity.CENTER_HORIZONTAL or Gravity.BOTTOM, 0, 0)
+        customtoast.duration = Toast.LENGTH_LONG
+
+        customtoast.show()
+    }
+
+    fun sucessoToast(text: String) {
+        val customToastroot = inflater.inflate(R.layout.custom_toast, null)
+        val customtoast = Toast(context)
+
+        val textView = customToastroot.findViewById<TextView>(R.id.textView1)
+        textView.text = text
+        textView.setTextColor(Color.rgb(0,255,0))
 
         customtoast.view = customToastroot
         customtoast.setGravity(Gravity.CENTER_HORIZONTAL or Gravity.BOTTOM, 0, 0)
